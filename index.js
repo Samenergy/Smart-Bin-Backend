0b5792c4a9138e0d4bdce26a0a48a0fa72978227
Samenergy <s.dushime@alustudent.com>
2024-06-18 23:26:58 +0200

    Done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://smart-bin-sigma.vercel.app",
   allowedHeaders: ["Authorization", "Content-Type"],
 };
 
